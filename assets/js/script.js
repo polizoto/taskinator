4dff5d7382f4c793886fd5eece51d72656d4da26
polizoto <polizoto@hotmail.com>
2021-12-01 09:42:16 -0500

    add data persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -223,41 +223,19 @@ var saveTasks = function() {
 
 var loadTasks = function() {
 // Gets task items from localStorage.
-tasks = localStorage.getItem("tasks")
-if (tasks === null) {
-  tasks = []
-  return false
+var savedTasks = localStorage.getItem("tasks")
+if (savedTasks === null) {
+  return false;
 }
-// Converts tasks from the string format back into an array of objects.
-tasks = JSON.parse(tasks)
-// Iterates through a tasks array and creates task elements on the page from it.
-for (var i = 0; i < tasks.length; i++) {
-  tasks[i].id = taskIdCounter
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.setAttribute("data-task-id", tasks[i].id)
-  var taskInfoEl = document.createElement("div");
-  taskInfoEl.className = "task-info";
-  taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
-  listItemEl.append(taskInfoEl)
-  var taskActionsEl = createTaskActions(tasks[i].id)
-  listItemEl.append(taskActionsEl)
-  if (tasks[i].status === "to do") {
-    listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
-    tasksToDoEl.append(listItemEl)
-  }
-  else if (tasks[i].status === "in progress") {
-    listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
-    tasksInProgressEl.append(listItemEl)
-  }
-  else if (tasks[i].status === "complete") {
-    listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
-    tasksCompletedEl.append(listItemEl)
-  }
-  taskIdCounter++;
+
+savedTasks = JSON.parse(savedTasks);
+// loop through savedTasks array
+for (var i = 0; i < savedTasks.length; i++) {
+  // pass each task object into the `createTaskEl()` function
+  createTaskEl(savedTasks[i]);
 }
 
 }
-
+  loadTasks();
   pageContentEl.addEventListener("click", taskButtonHandler);
   pageContentEl.addEventListener("change", taskStatusChangeHandler);
